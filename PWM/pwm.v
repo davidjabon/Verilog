bf2049932df0365a1d8b56230ab583f28ca9e321
davidjabon <djabon@depaul.edu>
2015-05-22 10:39:14 -0500

    PWM

---
+++ PWM/pwm.v
@@ -0,0 +1,69 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/22/2015 09:35:04 AM
+// Design Name: 
+// Module Name: pwm
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm #(CLK_PERIOD = 1000)(
+    input clk,
+    input enable,
+    input [31:0] pwm_period,
+    output out
+    );
+    
+reg clk_out=1'b0;
+reg [31:0] counter = 32'b0;
+
+always @(posedge clk)
+begin
+  if (enable == 1'b1)
+    begin
+      if (counter < pwm_period)
+        begin
+         clk_out <= 1'b1;
+         counter <= counter + 32'b1;
+        end
+      else
+        begin
+        if (counter < (CLK_PERIOD-1))
+         begin
+          clk_out <= 1'b0;
+          counter <= counter + 32'b1;
+         end
+        else 
+         begin
+          clk_out <= 1'b0;
+          counter <= 32'b0;
+         end
+       end //end if counter < pwm_period
+      
+     end   //end if(enable)
+   else
+     begin
+      clk_out =1'b0;
+     end 
+   
+end 
+
+assign out = clk_out;
+    
+endmodule
+
+
+
---
+++ PWM/pwm_tb.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/22/2015 09:52:09 AM
+// Design Name: 
+// Module Name: pwm_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pwm_tb();
+    reg clk;
+    wire out;
+   
+pwm  #(.CLK_PERIOD(20)) uut (.clk(clk),.enable(1'b1),.pwm_period(13),.out(out));
+
+initial
+    begin
+        clk=1'b0;
+    end
+    
+always #10 clk = ~clk;   
+   
+endmodule
