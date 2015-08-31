d215029afb1630e0be8ca445b77885147dca4a52
davidjabon <djabon@depaul.edu>
2015-08-31 11:23:30 -0500

    formatting changes seven_segment_leds

--- Seven_Segment_LED_drivers/seven_segment_leds_x_4_no_leading_zeros.v
+++ Seven_Segment_LED_drivers/seven_segment_leds_x_4_no_leading_zeros.v
@@ -21,7 +21,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 module seven_segment_leds_x_4_no_leading_zeros(
     input [15:0] bcd_in,
-	 input [3:0] decimal_points,
+    input [3:0] decimal_points,
     input clk,
     output  reg [6:0] a_to_g,
     output reg decimal_point,
@@ -38,42 +38,42 @@ assign counter = clkdiv[19:18];   //count every 2.6 ms  (with 100 MHz clock)
 //quad 4 to 1 MUX
 always @(*)
     case(counter)
-	    0: {digit, decimal_point} = {bcd_in[3:0], decimal_points[0]};
-		 1: {digit, decimal_point} = {bcd_in[7:4], decimal_points[1]};
-       2: {digit, decimal_point} = {bcd_in[11:8], decimal_points[2]};
-		 3: {digit, decimal_point} = {bcd_in[15:12], decimal_points[3]};
+	0: {digit, decimal_point} = {bcd_in[3:0], decimal_points[0]};
+	1: {digit, decimal_point} = {bcd_in[7:4], decimal_points[1]};
+        2: {digit, decimal_point} = {bcd_in[11:8], decimal_points[2]};
+        3: {digit, decimal_point} = {bcd_in[15:12], decimal_points[3]};
      endcase
 
 //7-segment decoder 
 always @(*)
      case(digit)
-	   0: a_to_g = 7'b0000001;
-		1: a_to_g = 7'b1001111;
-		2: a_to_g = 7'b0010010;
-		3: a_to_g = 7'b0000110;
-		4: a_to_g = 7'b1001100;
-		5: a_to_g = 7'b0100100;
-		6: a_to_g = 7'b0100000;
-		7: a_to_g = 7'b0001111;
-		8: a_to_g = 7'b0000000;
-		9: a_to_g = 7'b0000100;
-		default: a_to_g = 7'b0000001;  //default to 0
+	0: a_to_g = 7'b0000001;
+	1: a_to_g = 7'b1001111;
+	2: a_to_g = 7'b0010010;
+	3: a_to_g = 7'b0000110;
+	4: a_to_g = 7'b1001100;
+	5: a_to_g = 7'b0100100;
+	6: a_to_g = 7'b0100000;
+	7: a_to_g = 7'b0001111;
+	8: a_to_g = 7'b0000000;
+	9: a_to_g = 7'b0000100;
+	default: a_to_g = 7'b0000001;  //default to 0
      endcase
 	  
 //digit selector
 always @(*)
     case(counter)
-	   0: anode = 4'b1110;
-		1: anode = {2'b11,!(| bcd_in[15:4]),1'b1};
-		2: anode = {1'b1,!(| bcd_in[15:8]),2'b11};
-		3: anode = {!(| bcd_in[15:12]),3'b111};
-		default: anode = 4'b1111;  //all blank
+	0: anode = 4'b1110;
+	1: anode = {2'b11,!(| bcd_in[15:4]),1'b1};
+	2: anode = {1'b1,!(| bcd_in[15:8]),2'b11};
+	3: anode = {!(| bcd_in[15:12]),3'b111};
+	default: anode = 4'b1111;  //all blank
      endcase
      
 //clock divider 
 always  @ (posedge clk)
    begin
-	   clkdiv <= clkdiv + 20'b1;
-	end 
+	clkdiv <= clkdiv + 20'b1;
+   end 
 	  
 endmodule
